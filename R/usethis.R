55edd85c2d3ec4218e5f7547d169c8da7314e4d3
Katherine Kerwin <kkerwin@emory.edu>
2023-08-14 20:18:18 +0000

    use this feature

---
+++ R/usethis.R
@@ -0,0 +1,7 @@
+install.packages("usethis")
+usethis::use_git_config(user.name = "Katherine Kerwin", user.email = "kkerwin@emory.edu")
+usethis::create_github_token()
+gitcreds::gitcreds_set()
+
+install.packages("here")
+
